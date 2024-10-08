456dad19245845f0ae1b2e44380ecd2651b98913
Eva Viviani <evaviviani@Evas-MBP.home>
2024-10-08 12:54:11 +0200

    update R script to plot circular histograms

--- notebooks/plot_circular_histograms.R
+++ notebooks/plot_circular_histograms.R
@@ -1,27 +1,43 @@
 # -------------------------------------------------------------- #
 #
-# This script takes as input `circular_histograms_data.csv` and
+# This script takes as input `spider_plot_data-model19k.csv` and
 # it produces as output a series of circular histograms used
-# to represent the proportion of topics within a book
+# to represent the proportion of topics within a book/novel.
 #
+# This script is used for producing circular histograms for the paper
+# "From review to Genre to Novel and Back:
+# An attempt to relate reader impact to phenomena of novel text"
+#
+# Koolen et al., 2024 JCLS
 # -------------------------------------------------------------- #
+#
+# Created by Eva Viviani, and updated on the 8th of October 2024
 
 #### load libraries ####
 library(tidyverse)
 library(fmsb)
 
 #### load data ####
-read.csv('circular_histograms_data.csv', stringsAsFactors = F, na.strings = '') -> temp
+read.csv('spider_plot_data-model19k.csv', stringsAsFactors = F, na.strings = '') -> temp
 temp$isbn <- as.factor(temp$isbn)
 temp$topic_number <- as.factor(temp$topic_number)
 
-temp <- dplyr::mutate_all(temp, ~tidyr::replace_na(., 'unclassified'))
+## last minute changes to the spelling of some of the labels of `nur_genre`, `topic_proportions` and `category` ##
+temp$nur_genre <- as.character(temp$nur_genre)
+temp <- temp |>
+  mutate(nur_genre = if_else(nur_genre == "Romanticism", "Romance", nur_genre))
+
+temp <- temp |>
+  mutate(category = if_else(category == "behaviours / feelings", "behaviors / feelings", category))
+
+# tidy-up dataframe
+temp <- dplyr::mutate_all(temp, ~tidyr::replace_na(., 'unknown'))
 temp$nur_genre <- as.factor(temp$nur_genre)
 temp$category <- as.factor(temp$category)
-summary(temp)
 
-head(temp)
+colnames(temp)[colnames(temp)=='topic_proportions'] <- 'topic_proportion'
 
+temp <- unique(temp)
 genres <- unique(temp$nur_genre)
 
 #### pre-process data for making it usable by the plot ####
@@ -39,9 +55,13 @@ adjusted_df <- temp |>
   ungroup() |>
   mutate(adjusted_prop = topic_proportion / count) |>
   group_by(isbn, nur_genre) |>
-  mutate(total = sum(adjusted_prop))
+  mutate(
+    adjusted_prop = adjusted_prop / sum(adjusted_prop),
+    total = sum(adjusted_prop)
+  )
 
-
+# count how many books are per topic
+# we may want to plot this info in the title
 labels <- adjusted_df |>
   group_by(nur_genre)|>
   summarise(book_count = n_distinct(isbn)) |>
@@ -49,6 +69,7 @@ labels <- adjusted_df |>
 
 # to make the circular plots, we need a matrix.
 # lines below make the dataframe "squared" into a matrix
+# so that it becomes a suitable input for the circular plot function
 prop_df_genre <- tibble()
 for (genre in genres) {
   
@@ -64,7 +85,7 @@ for (genre in genres) {
   )
 }
 
-# replace na with 0 -- this makes sense in a barplot context
+# replace na with 0 -- this makes sense in a barplot context (i.e. these are interpreted as not present)
 prop_df_genre <- dplyr::mutate_all(prop_df_genre, ~tidyr::replace_na(., 0))
 prop_df_genre <- as.data.frame(prop_df_genre)
 rownames(prop_df_genre) <- prop_df_genre$genre
@@ -73,6 +94,9 @@ prop_df_genre$genre <- NULL
 head(prop_df_genre)
 
 #### set colours for the histograms ####
+# note that we set both border and "filling" colours
+# one rgb() per genre
+
 colors_border = c(
   rgb(0.2, 0.5, 0.5, 0.9),
   rgb(0.8, 0.2, 0.5, 0.9),
@@ -84,7 +108,8 @@ colors_border = c(
   rgb(0.5, 0.8, 0.4, 0.9),
   rgb(0.4, 0.4, 0.8, 0.9),
   rgb(0.6, 0.6, 0.3, 0.9),
-  rgb(0.5, 0.5, 0.5, 0.9)
+  rgb(0.5, 0.5, 0.5, 0.9),
+  rgb(0.9, 0.5, 0.2, 0.9)
 )
 
 colors_in = c(
@@ -98,26 +123,28 @@ colors_in = c(
   rgb(0.5, 0.8, 0.4, 0.4),
   rgb(0.4, 0.4, 0.8, 0.4),
   rgb(0.6, 0.6, 0.3, 0.4),
-  rgb(0.5, 0.5, 0.5, 0.4)
+  rgb(0.5, 0.5, 0.5, 0.4),
+  rgb(0.9, 0.5, 0.2, 0.4)
 )
 
-#### set labels for the histograms ####
+
+###### set labels for the histograms ###### 
 plots_list <- vector("list", nrow(prop_df_genre))
-labels$ordering_factor <- factor(labels$nur_genre, levels = c("unclassified", "Suspense", "Young_adult", "Historical_fiction", 
-                                                              "Literary_fiction", "Literary_thriller", "Children_fiction", 
-                                                              "Other fiction", "Romanticism", "Non-fiction", "Fantasy_fiction"))
+labels$ordering_factor <- factor(labels$nur_genre, levels = rownames(prop_df_genre))
 
 labels <- labels[order(labels$ordering_factor), ]
 
 labels$ordering_factor <- NULL
 
 #### produce and save the plots in a for loop ####
-prop_df_genre <- prop_df_genre[1:11,] 
+prop_df_genre_no_unclassified <- prop_df_genre
 
-for (i in (1:nrow(prop_df_genre))) {
-  png(paste0("radar_plot_", labels$nur_genre[i], ".png"), width = 6000, height = 3500, res=300)
+folder_where_i_save_my_plots <- 'circular_histograms/19kbooks/'
+
+for (i in (1:nrow(prop_df_genre_no_unclassified))) {
+  png(paste0(folder_where_i_save_my_plots, "radar_plot_", labels$nur_genre[i], ".png"), width = 2000, height = 1200, res=100)
   print(labels$nur_genre[i])
-  genre_df <- prop_df_genre[i,]
+  genre_df <- prop_df_genre_no_unclassified[i,]
   
   # Extract the maximum value for axis scaling
   max_val <- max(genre_df)
@@ -127,19 +154,22 @@ for (i in (1:nrow(prop_df_genre))) {
                      rep(min_val, ncol(genre_df)), 
                      genre_df) # add min and max
   
-  radarchart(genre_df_,                # Data for the first genre
+  # circular plot function
+  radarchart(genre_df_,                
              axistype = 1,
              pcol = colors_border[i], 
              pfcol = colors_in[i], 
-             plwd = 4, 
+             plwd = 5, 
              plty = 1,
              cglcol = "grey", 
              cglty = 1, 
              axislabcol = "grey",
              caxislabels=c("", "", "", "", paste0(round(max_val, 1)*100, "%")),
-             cglwd = 0.8,
+             calcex= 2,
+             cglwd = 3,
              vlcex = 2)
   plots_list[[i]] <- recordPlot()
-  title(main = labels$combined_column[i])
+  # set title
+  title(main = gsub("_", " ", labels$nur_genre[i]), cex.main=5)
   dev.off()
 }
